28a6f46952bccb09d2c4b66a2f191f8b60f0b244
Jacob Harris <jake98harris@gmail.com>
2019-03-21 08:57:28 -0500

    Framework laying

--- CarDealership/Controllers/HomeController.cs
+++ CarDealership/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarDealership.Models.DataModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -15,13 +16,37 @@ namespace CarDealership.Controllers
             throw new NotImplementedException();
         }
 
+        [HttpGet]
         public IHttpActionResult Specials()
         {
+            //TODO: Create JSON Array of specials
             throw new NotImplementedException();
         }
 
-        public IHttpActionResult Contact()
+        [HttpPost]
+        public IHttpActionResult Contact([FromBody] Contact newContact)
         {
+            if(newContact == null)
+            {
+                return BadRequest("Could not parse from form.");
+            }
+            if (string.IsNullOrWhiteSpace(newContact.ContactName))
+            {
+                return BadRequest("Please enter your name.");
+            }
+
+            if (string.IsNullOrWhiteSpace(newContact.ContactMessage))
+            {
+                return BadRequest("Please enter a message.");
+            }
+
+            if(string.IsNullOrWhiteSpace(newContact.Email) && string.IsNullOrWhiteSpace(newContact.Phone))
+            {
+                return BadRequest("Please provide a means of contact.");
+            }
+
+            newContact.ContactID = 0;
+            //TODO: Store new contact object
             throw new NotImplementedException();
         }
     }

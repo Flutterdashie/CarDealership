1d0001eea239e4e373141fa9c95ddb321cf47de5
Jacob Harris <jake98harris@gmail.com>
2019-03-22 15:51:57 -0500

    The stage is set, time for the show!

--- CarDealership/Controllers/AllViewController.cs
+++ CarDealership/Controllers/AllViewController.cs
@@ -9,9 +9,159 @@ namespace CarDealership.Controllers
     public class AllViewController : Controller
     {
         // GET: AllView
+        [Route("Home/Index")]
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Inventory/New")]
+        public ActionResult New()
+        {
+            //TODO: AJAX
+            throw new NotImplementedException();
+        }
+
+        [Route("Inventory/Used")]
+        public ActionResult Used()
+        {
+            //TODO: AJAX
+            throw new NotImplementedException();
+        }
+
+        [Route("Inventory/Details/{id}")]
+        public ActionResult Details(int id)
         {
             throw new NotImplementedException();
         }
+
+        [Route("Home/Specials")]
+        public ActionResult Specials()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("Home/Contact")]
+        public ActionResult Contact()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Sales")]
+        [Route("Sales/Index")]
+        public ActionResult SalesIndex()
+        {
+            //TODO: AJAX
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Sales")]
+        [Route("Sales/Purchase/{id}")]
+        public ActionResult Purchase(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [Route("Admin/Vehicles")]
+        public ActionResult Vehicles()
+        {
+            //TODO: AJAX for full results
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [Route("Admin/AddVehicle")]
+        public ActionResult AddVehicle()
+        {
+            //TODO: AJAX for models/makes
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [Route("Admin/EditVehicle/{id}")]
+        public ActionResult EditVehicle(int id)
+        {
+            //TODO: AJAX for models/makes
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [Route("Admin/Users")]
+        public ActionResult Users()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [Route("Admin/AddUser")]
+        public ActionResult AddUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [Route("Admin/EditUser")]
+        public ActionResult EditUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Admin,Sales")]
+        [Route("Account/ChangePassword")]
+        public ActionResult ChangePassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [Route("Admin/Makes")]
+        public ActionResult Makes()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [Route("Admin/Models")]
+        public ActionResult Models()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [Route("Admin/Specials")]
+        public ActionResult AdminSpecials()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [Route("Reports/Index")]
+        public ActionResult ReportsIndex()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [Route("Reports/Sales")]
+        public ActionResult ReportsSales()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [Route("Reports/Inventory")]
+        public ActionResult Inventory()
+        {
+            throw new NotImplementedException();
+        }
+
+        [AllowAnonymous]
+        [Route("Account/Login")]
+        public ActionResult Login()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
